b1eb35da0818569cb80cf5f51013870da00f2019
Mohammad Bazargan <jc.mhmd.bazargan850627@gmail.com>
2025-09-22 19:10:42 +0330

    Fixed icons, Added edit button

--- src/script.js
+++ src/script.js
@@ -20,19 +20,42 @@ addTaskBtn.addEventListener("click", () => {
     const actions = document.createElement("div");
     actions.className = "flex gap-2";
 
+    // Complete Button
     const taskCompleteBtn = document.createElement("button");
     taskCompleteBtn.innerHTML =
         '<img src="../icons/check-icon.svg" alt="done" width="18px" height="18px" />';
     taskCompleteBtn.className =
         "px-2 py-1 bg-green-600 text-white rounded hover:bg-green-500";
-
     taskCompleteBtn.addEventListener("click", () => {
         span.classList.toggle("line-through");
         span.classList.toggle("text-gray-500");
     });
 
+    // Edit Button
+    const taskEditBtn = document.createElement("button");
+    taskEditBtn.innerHTML = '<img src="../icons/pencil-icon.svg" alt="edit" width="18px" height="18px" />';
+    taskEditBtn.className = "px-2 py-1 bg-yellow-600 text-white rounded hover:bg-yellow-500";
+    taskEditBtn.addEventListener("click", () => {
+        // TODO: When clicking the button, edit the task in a popup window?!
+        return;
+    })
+
+    // Delete Button
+    const taskDeleteBtn = document.createElement("button");
+    taskDeleteBtn.innerHTML =
+        '<img src="../icons/trash-icon.svg" alt="delete" width="18px" height="18px" />';
+    taskDeleteBtn.className =
+        "px-2 py-1 bg-red-600 text-white rounded hover:bg-red-500";
+    taskDeleteBtn.addEventListener("click", () => {
+        taskList.removeChild(taskItem);
+    });
+
     actions.appendChild(taskCompleteBtn);
+    actions.appendChild(taskEditBtn);
+    actions.appendChild(taskDeleteBtn);
     taskItem.appendChild(span);
     taskItem.appendChild(actions);
     taskList.appendChild(taskItem);
+
+    taskInput.value = null;
 });
